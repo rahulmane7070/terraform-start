9724692572942aadcc601d8984bf57b6e8ec435d
Administrator <sachinna@cybage.com>
2018-04-10 16:58:52 +0530

    first terraform hands on

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "security_group_id" {default = "sg-b51a8cc4"}
+
+variable "key_name" {default = "terraform" }
+variable "public_key" {default = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA35N5MD355b6YCYHUOJifJrM6oHakmS/auLMmQ2KoEySN4WkojCTcKVYsAguD7yNe9eRTxQMLacJ3krdCbsNbPe8Nf0jvG+Amn24BVFrfwfx8sZCPMXW4+3+Adgzyl3GT9r/TjMOEgbAmQmKzgeqpdC4Zr9mJ63RTNLwNWrYcJB7qKfMG8pTW/CkoatPpuclG2SONO3NqKuP/EODz641C9VPxGkI1vH6L1843u6FBrRmZNbmv+m5R7/ERGnsib3CY2Ftq5uBM4LwWS3NEmEjhIvrV0rzbSwlxIo52HPljkyMkTr0ATlds8RBG0r1jgXI5UPrqlVb41XAxh3QHxz380QIDAQAB"}
